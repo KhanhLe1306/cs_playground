3efbf4627cfe5599f6128cefccc7227b46d5f62c
Khanh Le <khanhle@unomaha.edu>
2022-02-06 21:01:01 -0600

    working on merge 2 sorted lists

---
+++ Merge_Two_Sorted_LinkedLists/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Merge_Two_Sorted_LinkedLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            ListNode node1 = new ListNode(1);
+            ListNode node2 = new ListNode(2);
+            ListNode node3 = new ListNode(4);
+
+            ListNode node4 = new ListNode(1);
+            ListNode node5 = new ListNode(3);
+            ListNode node6 = new ListNode(4);
+
+            node1.next = node2;
+            node2.next = node3;
+
+            node4.next = node5;
+            node5.next = node6;
+
+            ListNode list1 = node1;
+            ListNode list2 = node4;
+
+            ListNode result = merge(list1, list2);
+
+            // Console.WriteLine(result.val);
+            // Console.WriteLine("Hello");
+        }
+
+        static ListNode merge(ListNode list1, ListNode list2)
+        {
+
+            ListNode result = new ListNode();
+            ListNode p1 = list1;
+            ListNode p2 = list2;
+
+            // Console.WriteLine("p1.val " + p1.val);
+            // Console.WriteLine("p2.val " + p2.val);
+            while (p1.val != 0 || p2.val != 0)
+            {
+                if (p1.val >= p2.val)
+                {
+                    Console.WriteLine("Inside if: ");
+                    Console.WriteLine("p1.val " + p1.val);
+                    Console.WriteLine("p2.val " + p2.val);
+                    result.val = p2.val;
+                    p2 = p2.next;
+                }
+                else
+                {
+                    Console.WriteLine("Inside else: ");
+                    Console.WriteLine("p1.val " + p1.val);
+                    Console.WriteLine("p2.val " + p2.val);
+                    result.val = p1.val;
+                    p1.val = p1.val;
+                }
+            }
+
+            return result;
+
+        }
+    }
+
+    class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ Valid_Parentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace Valid_Parentheses
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string s = "}{";
+            bool res = IsValid(s);
+            Console.WriteLine(res);
+        }
+
+        public static bool IsValid(string s)
+        {
+            Stack stack = new Stack();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                switch (c)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        stack.Push(c);
+                        break;
+                    case ')':
+                        if (stack.Count != 0 && stack.Pop().ToString() == "(")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    case '}':
+                        if (stack.Count != 0 && stack.Pop().ToString() == "{")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    case ']':
+                        if (stack.Count != 0 && stack.Pop().ToString() == "[")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                }
+            }
+            return true;
+        }
+    }
+}
